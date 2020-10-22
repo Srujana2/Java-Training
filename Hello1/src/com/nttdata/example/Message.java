a27079cf19441d46ee4b5135204fc8ad62e6c511
SRUJANA <SRUJANA@DESKTOP-KG5UD08>
2020-10-22 19:18:55 +0530

    Class is created

---
+++ Hello1/src/com/nttdata/example/Message.java
@@ -0,0 +1,10 @@
+package com.nttdata.example;
+
+public class Message {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome yo core java");
+	}
+
+}
